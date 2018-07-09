9129887aed537fe9296232159b769674e2ed0d13
Felipe Roveran <feliperoveran@hotmail.com>
2018-07-09 16:38:13 -0300

    create a store from json

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,14 @@
 class Store < ApplicationRecord
   validates_presence_of :name, :owner, :document, :coverage_area, :address
+
+  def self.from_json!(json)
+    create!(
+      id: json['id'],
+      name: json['tradingName'],
+      owner: json['ownerName'],
+      document: json['document'],
+      coverage_area: RGeo::GeoJSON.decode(json['coverageArea']),
+      address: RGeo::GeoJSON.decode(json['address'])
+    )
+  end
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -6,4 +6,37 @@ RSpec.describe Store, type: :model do
   it { should validate_presence_of :document }
   it { should validate_presence_of :coverage_area }
   it { should validate_presence_of :address }
+
+  describe '.from_json!' do
+    it 'creates a new Store' do
+      json = JSON.parse file_fixture('store.json').read
+
+      expect {
+        described_class.from_json! json
+      }.to change { Store.count }.by 1
+    end
+
+    it 'creates a new store with the right attributes' do
+      json = JSON.parse file_fixture('store.json').read
+
+      described_class.from_json! json
+
+      expect(Store.last).to have_attributes(
+        id: json['id'].to_i,
+        name: json['tradingName'],
+        owner: json['ownerName'],
+        document: json['document'],
+        coverage_area: RGeo::GeoJSON.decode(json['coverageArea']),
+        address: RGeo::GeoJSON.decode(json['address'])
+      )
+    end
+
+    it 'raises ActiveRecord::RecordInvalid when the store is invalid' do
+      json = JSON.parse file_fixture('invalid_store.json').read
+
+      expect {
+        described_class.from_json! json
+      }.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
 end
