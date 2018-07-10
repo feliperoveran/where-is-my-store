7cebe93f4487473f90535c255066d7332c870f7b
Felipe Roveran <feliperoveran@hotmail.com>
2018-07-09 21:37:20 -0300

    create store factory

---
+++ spec/factories/store_factory.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :store do
+    name "Adega Osasco"
+    owner "Ze da Ambev"
+    document "02.453.716/000170"
+    coverage_area "MULTIPOLYGON (((0 0, 1 0, 1 1, 0 1, 0 0)))"
+    address "POINT (0.5 0.5)"
+  end
+end
