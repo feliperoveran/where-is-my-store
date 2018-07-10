93a29a0837153ea2b5dc507e75f968c4a92ee422
Felipe Roveran <feliperoveran@hotmail.com>
2018-07-10 00:16:52 -0300

    add stores db seeds

--- spec/factories/store_factory.rb
+++ spec/factories/store_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :store do
     name "Adega Osasco"
-    owner "Ze da Ambev"
+    owner "Ze da Esquina"
     document "02.453.716/000170"
     coverage_area "MULTIPOLYGON (((0 0, 1 0, 1 1, 0 1, 0 0)))"
     address "POINT (0.5 0.5)"
